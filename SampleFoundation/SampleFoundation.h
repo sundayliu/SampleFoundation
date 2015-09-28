b66a99041ae844c4be13ac4acd218ffd71791ee3
chameleonliu <martinkro@aliyun.com>
2015-09-28 10:57:08 +0800

    Initial Commit of Library

---
+++ SampleFoundation/SampleFoundation.h
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.h
+//  SampleFoundation
+//
+//  Created by gavingu on 15/9/28.
+//  Copyright (c) 2015年 tencent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleFoundation : NSObject
+
+@end
---
+++ SampleFoundation/SampleFoundation.m
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.m
+//  SampleFoundation
+//
+//  Created by gavingu on 15/9/28.
+//  Copyright (c) 2015年 tencent. All rights reserved.
+//
+
+#import "SampleFoundation.h"
+
+@implementation SampleFoundation
+
+@end
